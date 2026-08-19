8e38afdbf9f4c1698caff4b23817a64e4a301a46
oliviervanre <olivier.vanre@gmail.com>
2026-08-19 16:48:50 +0200

    Actualiser le cache du prototype

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "fluflu-v1";
+const CACHE = "fluflu-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
